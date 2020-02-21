321ca6f905d24ba42f98d421dcf027d6c747be75
nithyamani <nithya.mn999@gmail.com>
2020-02-21 16:14:21 +0530

    changes

--- src/test/java/local/StepDefs.java
+++ src/test/java/local/StepDefs.java
@@ -25,7 +25,6 @@ public class StepDefs {
     WebDriver driver;
     private static Local l;
     WebElement element;
-    private Local l;
 
     @Given("Open Browser")
     public void open_Browser() throws Exception {
@@ -36,51 +35,37 @@ public class StepDefs {
         caps.setCapability("browser_version", "75.0");
         caps.setCapability("build", "cucumber-java-testng-browserstack");
         caps.setCapability("name", "local_test");
-<<<<<<< HEAD
         caps.setCapability("browserstack.local", "true");
         //caps.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
 
-        if(caps.getCapability("browserstack.local") != null && caps.getCapability("browserstack.local") == "true"){
+        if (caps.getCapability("browserstack.local") != null && caps.getCapability("browserstack.local") == "true") {
             //System.out.println("INSIDE CODE BINDINGS");
             l = new Local();
-            System.out.println("Local object: "+l);
-=======
-        caps.setCapability("browserstack.local", browserstackLocal);//Note: the value for browserstack.local should be String and not boolean.
-        caps.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
-        
-        if(caps.getCapability("browserstack.local") != null && caps.getCapability("browserstack.local") == "true"){
-            l = new Local();
->>>>>>> 6baa80cc51a3edecf5a805c9ef4f6f5f29922e38
-            Map<String, String> options = new HashMap<String, String>();
-            options.put("key", AUTOMATE_KEY);
-            l.start(options);
+            System.out.println("Local object: " + l);
+            caps.setCapability("browserstack.local", browserstackLocal);//Note: the value for browserstack.local should be String and not boolean.
+            caps.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
+
+            if (caps.getCapability("browserstack.local") != null && caps.getCapability("browserstack.local") == "true") {
+                l = new Local();
+                Map<String, String> options = new HashMap<String, String>();
+                options.put("key", AUTOMATE_KEY);
+                l.start(options);
+            }
+            driver = new RemoteWebDriver(new URL(URL), caps);
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> 6baa80cc51a3edecf5a805c9ef4f6f5f29922e38
-        driver = new RemoteWebDriver(new URL(URL), caps);
     }
-
     @When("Go to localhost")
-    public void go_to_localhost() {
-<<<<<<< HEAD
-
-=======
->>>>>>> 6baa80cc51a3edecf5a805c9ef4f6f5f29922e38
+    public void go_to_localhost () {
         driver.get("http://bs-local.com:45691/check");
     }
 
     @Then("Retrieve Title if Up and Running")
-    public void retrieve_Title_if_Up_and_Running() throws Exception {
+    public void retrieve_Title_if_Up_and_Running () throws Exception {
         System.out.println(driver.getTitle());
         driver.quit();
-<<<<<<< HEAD
         if (l != null) {
             l.stop();
         }
-=======
-        if(l != null) l.stop();
->>>>>>> 6baa80cc51a3edecf5a805c9ef4f6f5f29922e38
+        if (l != null) l.stop();
     }
 }
